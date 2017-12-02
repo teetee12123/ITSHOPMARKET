503cbe9c355eb53782dbc6802ff6461711e49104
teetee12123 <34017794+teetee12123@users.noreply.github.com>
2017-12-02 14:51:48 +0700

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace OnlineShop
+{
+    public class ChatHub : Hub
+    {
+        public void BroadCastMessage()
+        {
+            Clients.All.Message(DateTime.Now);
+        }
+        public void MessageGET(string name,string message)
+        {
+            Clients.All.MessageSend(name, message);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OnlineShop.Startup))]
+
+namespace OnlineShop
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
